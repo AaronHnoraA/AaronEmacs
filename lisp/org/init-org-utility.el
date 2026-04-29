8ccac0da9743b3dc6006a649e35d6dd9219f695b
Aaron He <AaronHeDev@outlook.com>
2026-04-29 12:00:50 +1000

    .

--- lisp/org/init-org-utility.el
+++ lisp/org/init-org-utility.el
@@ -29,6 +29,7 @@
 (defvar org-download-image-dir)
 (defvar org-download-timestamp)
 (defvar org-roam-directory)
+(defvar my/org-reference--minibuffer-candidates nil)
 
 (declare-function consult--jump-preview "consult" ())
 (declare-function consult--read "consult" (table &rest options))
@@ -40,6 +41,7 @@
 (declare-function org-roam-node-id "org-roam-node" (node))
 (declare-function org-roam-node-read "org-roam-node" (&optional initial-input filter-fn sort-fn require-match prompt))
 (declare-function org-roam-node-title "org-roam-node" (node))
+(declare-function vertico--candidate "vertico" (&optional highlight))
 
 (defun my/org-reference--display-math-bounds ()
   "Return bounds of the surrounding Org \\[...\\] formula, or nil."
@@ -255,9 +257,15 @@ the user to fill in."
                   :file (org-roam-node-file node)
                   :id (org-roam-node-id node)
                   :title (org-roam-node-title node))))
-      (let ((file (read-file-name "Reference file: "
-                                  default-directory
-                                  nil t)))
+      (let* ((current-file (buffer-file-name (buffer-base-buffer)))
+             (file (read-file-name "Reference file: "
+                                   (or (and current-file
+                                            (file-name-directory current-file))
+                                       default-directory)
+                                   current-file
+                                   t
+                                   (and current-file
+                                        (file-name-nondirectory current-file)))))
         (list :kind 'file
               :file (expand-file-name file)
               :title (file-name-base file))))))
@@ -286,7 +294,7 @@ the user to fill in."
     (let ((line (my/org-reference--candidate-line)))
       (cond
        ((string-match-p "\\\\][ \t]*<<[^>\n]+>>[ \t]*\\'" line)
-        (let ((end (line-beginning-position))
+        (let ((end pos)
               begin)
           (when (search-backward "\\[" nil t)
             (setq begin (point)))
@@ -314,6 +322,71 @@ the user to fill in."
                (buffer-substring-no-properties begin end)
              line))))))))
 
+(defun my/org-reference--target-formula-preview (pos)
+  "Return nearby display math text before target at POS, or nil."
+  (save-excursion
+    (goto-char pos)
+    (when (search-backward "\\]" nil t)
+      (let ((end (point)))
+        (when (search-backward "\\[" nil t)
+          (my/org-reference--compact-text
+           (buffer-substring-no-properties (point) end)
+           140))))))
+
+(defun my/org-reference--formula-target-p (target preview)
+  "Return non-nil when TARGET/PREVIEW looks like a formula target."
+  (or (string-prefix-p "eq-" (or target ""))
+      (string-match-p "\\\\[([]" (or preview ""))))
+
+(defun my/org-reference--heading-scope (pos)
+  "Return outline path containing POS, or nil."
+  (save-excursion
+    (goto-char pos)
+    (when (org-back-to-heading t)
+      (my/org-reference--compact-text
+       (mapconcat #'identity (org-get-outline-path t t) " / ")
+       80))))
+
+(defun my/org-reference--block-scope (pos)
+  "Return nearest Org block type containing POS, or nil."
+  (save-excursion
+    (goto-char pos)
+    (let ((case-fold-search t)
+          begin type)
+      (when (re-search-backward "^[ \t]*#\\+begin_\\([[:alnum:]_-]+\\)\\b" nil t)
+        (setq begin (point)
+              type (downcase (match-string-no-properties 1)))
+        (when (and (< begin pos)
+                   (re-search-forward
+                    (format "^[ \t]*#\\+end_%s\\b" (regexp-quote type)) nil t)
+                   (<= pos (line-end-position)))
+          type)))))
+
+(defun my/org-reference--scan-block-scopes ()
+  "Return block scope candidates in the current Org buffer."
+  (let ((case-fold-search t)
+        scopes)
+    (save-excursion
+      (goto-char (point-min))
+      (while (re-search-forward "^[ \t]*#\\+begin_\\([[:alnum:]_-]+\\)\\b.*$" nil t)
+        (let* ((type (downcase (match-string-no-properties 1)))
+               (begin (line-beginning-position))
+               (label (format "%s block" type)))
+          (when (re-search-forward
+                 (format "^[ \t]*#\\+end_%s\\b" (regexp-quote type)) nil t)
+            (push (list :kind 'scope-block
+                        :target nil
+                        :label label
+                        :line (line-number-at-pos begin)
+                        :pos begin
+                        :begin begin
+                        :end (line-end-position)
+                        :block type
+                        :preview (my/org-reference--compact-text
+                                  (format "#+begin_%s" type)))
+                  scopes)))))
+    (nreverse scopes)))
+
 (defun my/org-reference--latex-comment-line-p (pos)
   "Return non-nil when POS is on a LaTeX comment-style line."
   (save-excursion
@@ -332,15 +405,19 @@ the user to fill in."
          (org-element-map tree 'headline
            (lambda (headline)
              (let* ((begin (org-element-property :begin headline))
+                    (end (org-element-property :end headline))
                     (raw-title (org-element-property :raw-value headline))
                     (id (org-element-property :ID headline))
                     (custom-id (org-element-property :CUSTOM_ID headline))
                     (level (org-element-property :level headline)))
-               (push (list :kind 'heading
+               (push (list :kind 'scope-heading
                            :target raw-title
                            :label raw-title
                            :line (line-number-at-pos begin)
                            :pos begin
+                           :begin begin
+                           :end end
+                           :level level
                            :preview (format "%s %s"
                                             (make-string (max 0 (1- level)) ?*)
                                             raw-title))
@@ -366,13 +443,23 @@ the user to fill in."
              (let ((value (org-element-property :value target))
                    (begin (org-element-property :begin target)))
                (unless (my/org-reference--latex-comment-line-p begin)
-                 (push (list :kind 'target
-                             :target value
-                             :label value
-                             :line (line-number-at-pos begin)
-                             :pos begin
-                             :preview (my/org-reference--nearby-preview begin))
-                       candidates))))))))
+                 (let* ((preview (or (my/org-reference--target-formula-preview begin)
+                                     (my/org-reference--nearby-preview begin)))
+                        (formula-p (my/org-reference--formula-target-p value preview))
+                        (heading-scope (my/org-reference--heading-scope begin))
+                        (block-scope (my/org-reference--block-scope begin)))
+                   (push (list :kind (if formula-p 'formula 'target)
+                               :target value
+                               :label (if formula-p preview value)
+                               :line (line-number-at-pos begin)
+                               :pos begin
+                               :begin begin
+                               :end begin
+                               :scope heading-scope
+                               :block block-scope
+                               :preview preview)
+                         candidates)))))))
+       (setq candidates (append candidates (my/org-reference--scan-block-scopes)))))
     (nreverse candidates)))
 
 (defun my/org-reference--scan-text-targets (file)
@@ -418,22 +505,83 @@ the user to fill in."
                 :preview (format "File: %s" (file-name-nondirectory file)))
           targets)))
 
-(defun my/org-reference--target-candidates (file)
-  "Return propertized completion candidates for reference targets in FILE."
+(defun my/org-reference--candidate-in-scope-p (candidate scope)
+  "Return non-nil when CANDIDATE is inside SCOPE."
+  (let ((pos (plist-get candidate :pos))
+        (begin (plist-get scope :begin))
+        (end (plist-get scope :end)))
+    (and pos begin end
+         (< begin pos)
+         (<= pos end))))
+
+(defun my/org-reference--filter-scope-children (candidates scope)
+  "Return immediate useful children of SCOPE from CANDIDATES."
+  (let* ((scope-kind (plist-get scope :kind))
+         (scope-level (plist-get scope :level))
+         (scope-begin (plist-get scope :begin))
+         (children (seq-filter
+                    (lambda (candidate)
+                      (and (not (eq candidate scope))
+                           (not (= (or (plist-get candidate :pos) -1)
+                                   (or scope-begin -2)))
+                           (my/org-reference--candidate-in-scope-p candidate scope)))
+                    candidates)))
+    (if (eq scope-kind 'scope-heading)
+        (seq-filter
+         (lambda (candidate)
+           (let ((kind (plist-get candidate :kind)))
+             (not (and (eq kind 'scope-heading)
+                       (<= (or (plist-get candidate :level) most-positive-fixnum)
+                           (or scope-level 0))))))
+         children)
+      children)))
+
+(defun my/org-reference--scope-candidate-p (target)
+  "Return non-nil when TARGET can be drilled into."
+  (memq (plist-get target :kind) '(scope-heading scope-block)))
+
+(defun my/org-reference--target-candidates (file &optional targets)
+  "Return propertized completion candidates for reference targets in FILE.
+When TARGETS is non-nil, use that candidate plist list instead of scanning FILE."
   (mapcar
    (lambda (target)
      (propertize
-      (format "%-9s %5s  %-40s  %s"
-              (plist-get target :kind)
-              (plist-get target :line)
-              (my/org-reference--compact-text
-               (or (plist-get target :label) "")
-               40)
-              (my/org-reference--compact-text
-               (or (plist-get target :preview) "")
-               90))
+      (let ((kind (plist-get target :kind)))
+        (cond
+         ((eq kind 'formula)
+            (format "%-9s %5s  %-28s  %-12s  %-64s  %s"
+                    kind
+                    (plist-get target :line)
+                    (my/org-reference--compact-text
+                     (or (plist-get target :scope) "")
+                     28)
+                    (my/org-reference--compact-text
+                     (or (plist-get target :block) "")
+                     12)
+                    (my/org-reference--compact-text
+                     (or (plist-get target :preview) "")
+                     64)
+                    (or (plist-get target :target) "")))
+         ((memq kind '(scope-heading scope-block))
+          (format "%-9s %5s  %-40s  %s"
+                  (if (eq kind 'scope-heading) 'heading 'block)
+                  (plist-get target :line)
+                  (my/org-reference--compact-text
+                   (or (plist-get target :label) "")
+                   40)
+                  "TAB to enter"))
+         (t
+          (format "%-9s %5s  %-40s  %s"
+                  kind
+                  (plist-get target :line)
+                  (my/org-reference--compact-text
+                   (or (plist-get target :label) "")
+                   40)
+                  (my/org-reference--compact-text
+                   (or (plist-get target :preview) "")
+                   90)))))
       'my/org-reference-target target))
-   (my/org-reference--file-targets file)))
+   (or targets (my/org-reference--file-targets file))))
 
 (defun my/org-reference--target-preview-state (file)
   "Return Consult preview state for candidates in FILE."
@@ -451,27 +599,96 @@ the user to fill in."
                             (set-marker marker pos (find-file-noselect file))
                             marker)))))))))
 
+(defun my/org-reference--minibuffer-selected-target ()
+  "Return the current target plist selected in the minibuffer."
+  (let* ((selected (cond
+                    ((fboundp 'vertico--candidate)
+                     (vertico--candidate))
+                    (t
+                     (minibuffer-contents))))
+         (target (and selected
+                      (get-text-property 0 'my/org-reference-target selected))))
+    (or target
+        (and selected
+             (get-text-property
+              0 'my/org-reference-target
+              (seq-find (lambda (candidate)
+                          (string= (substring-no-properties candidate)
+                                   (substring-no-properties selected)))
+                        my/org-reference--minibuffer-candidates))))))
+
+(defun my/org-reference--drill-minibuffer ()
+  "Enter the selected heading/block scope from the reference minibuffer."
+  (interactive)
+  (let ((target (my/org-reference--minibuffer-selected-target)))
+    (cond
+     ((my/org-reference--scope-candidate-p target)
+      (throw 'my/org-reference-drill target))
+     (target
+      (message "This candidate is a target; press RET to insert it"))
+     (t
+      (message "No target selected")))))
+
+(defun my/org-reference--target-read-keymap ()
+  "Return keymap used by the target browser."
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "TAB") #'my/org-reference--drill-minibuffer)
+    (define-key map (kbd "<tab>") #'my/org-reference--drill-minibuffer)
+    map))
+
+(defun my/org-reference--read-target-from-candidates (file candidates prompt)
+  "Read one target from CANDIDATES in FILE using PROMPT."
+  (let ((formatted (my/org-reference--target-candidates file candidates)))
+    (unless formatted
+      (user-error "No reference targets found in this scope"))
+    (let ((my/org-reference--minibuffer-candidates formatted))
+      (if (fboundp 'consult--read)
+          (progn
+            (require 'consult)
+            (consult--read formatted
+                           :prompt prompt
+                           :require-match t
+                           :state (my/org-reference--target-preview-state file)
+                           :preview-key '(:debounce 0.15 any)
+                           :keymap (my/org-reference--target-read-keymap)
+                           :lookup (apply-partially
+                                    #'consult--lookup-prop
+                                    'my/org-reference-target)))
+        (let ((choice (completing-read prompt formatted nil t)))
+          (or (get-text-property 0 'my/org-reference-target choice)
+              (user-error "No target selected")))))))
+
 (defun my/org-reference--read-target (file)
   "Read one reference target from FILE."
-  (let ((candidates (my/org-reference--target-candidates file)))
-    (unless candidates
+  (let* ((all-candidates (my/org-reference--file-targets file))
+         (current all-candidates)
+         (scope-labels nil))
+    (unless all-candidates
       (user-error "No reference targets found in %s" file))
-    (let ((choice
-           (if (fboundp 'consult--read)
-               (progn
-                 (require 'consult)
-                 (consult--read candidates
-                                :prompt "Target: "
-                                :require-match t
-                                :state (my/org-reference--target-preview-state file)
-                                :preview-key '(:debounce 0.15 any)
-                                :lookup (apply-partially
-                                         #'consult--lookup-prop
-                                         'my/org-reference-target)))
-             (completing-read "Target: " candidates nil t))))
-      (or (and (listp choice) choice)
-          (get-text-property 0 'my/org-reference-target choice)
-          (user-error "No target selected")))))
+    (catch 'done
+      (while t
+        (let* ((prompt (if scope-labels
+                           (format "Target [%s]: "
+                                   (mapconcat #'identity
+                                              (reverse scope-labels)
+                                              " / "))
+                         "Target: "))
+               (choice
+                (catch 'my/org-reference-drill
+                  (my/org-reference--read-target-from-candidates
+                   file current prompt))))
+          (if (my/org-reference--scope-candidate-p choice)
+              (let ((children (my/org-reference--filter-scope-children
+                               all-candidates choice)))
+                (unless children
+                  (user-error "No targets under %s"
+                              (plist-get choice :label)))
+                (setq current children
+                      scope-labels (cons (plist-get choice :label)
+                                         scope-labels)))
+            (throw 'done
+                   (or (and (listp choice) choice)
+                       (user-error "No target selected")))))))))
 
 (defun my/org-reference--relative-file (file)
   "Return FILE path relative to the current buffer when possible."
@@ -484,11 +701,21 @@ the user to fill in."
   (pcase (plist-get target :kind)
     ('file nil)
     ('id (concat "*" (plist-get target :label)))
+    ('scope-heading (concat "*" (plist-get target :target)))
     ('heading (concat "*" (plist-get target :target)))
     ('custom-id (concat "#" (plist-get target :target)))
+    ('formula (plist-get target :target))
     ('target (plist-get target :target))
     ('line (number-to-string (plist-get target :line)))
     (_ nil)))
+
+(defun my/org-reference--same-file-p (file)
+  "Return non-nil when FILE is the current buffer file."
+  (let ((current-file (buffer-file-name (buffer-base-buffer))))
+    (and current-file
+         file
+         (file-equal-p (expand-file-name file)
+                       (expand-file-name current-file)))))
 
 (defun my/org-reference--build-link (source target description)
   "Build an Org link for SOURCE plist, TARGET plist and DESCRIPTION."
@@ -502,9 +729,12 @@ the user to fill in."
                       (if (eq (plist-get target :kind) 'id)
                           (plist-get target :target)
                         (plist-get source :id)))
-            (concat "file:" (my/org-reference--relative-file (plist-get source :file))
-                    (when search
-                      (concat "::" search))))))
+            (if (and search
+                     (my/org-reference--same-file-p (plist-get source :file)))
+                (or search "")
+              (concat "file:" (my/org-reference--relative-file (plist-get source :file))
+                      (when search
+                        (concat "::" search)))))))
     (if desc
         (org-link-make-string raw desc)
       (org-link-make-string raw))))
